07a3200cc48ce23282e4174e4f7852a8962165d8
martin <martinmichuki8@gmail.com>
2022-09-13 18:19:18 +0300

    Updated project

--- app/src/main/java/com/michtech/pointofSale/fragments/Store.java
+++ app/src/main/java/com/michtech/pointofSale/fragments/Store.java
@@ -2,8 +2,6 @@ package com.michtech.pointofSale.fragments;
 
 import android.annotation.SuppressLint;
 import android.content.Intent;
-import android.graphics.Bitmap;
-import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.os.Handler;
