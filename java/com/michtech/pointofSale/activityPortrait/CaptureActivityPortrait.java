44d6e5adb1c7e29710d3eba06f605104cdc29b40
Martin Mich <61991142+martinmichuki8@users.noreply.github.com>
2022-09-13 18:23:49 +0300

    Delete java/com/michtech/pointofSale directory

--- java/com/michtech/pointofSale/activityPortrait/CaptureActivityPortrait.java
+++
@@ -1,6 +0,0 @@
-package com.michtech.pointofSale.activityPortrait;
-
-import com.journeyapps.barcodescanner.CaptureActivity;
-
-public class CaptureActivityPortrait extends CaptureActivity {
-}
--- java/com/michtech/pointofSale/background/SomeRequest.java
+++
@@ -1,4 +0,0 @@
-package com.michtech.pointofSale.background;
-public class SomeRequest{
-    public String text;
-}
